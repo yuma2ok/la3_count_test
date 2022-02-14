5d76681b0ed79226de47f003b19db86d70301fbe
yuma2ok <yuma.matsuoka25@gmail.com>
2022-02-14 05:43:47 +0000

    second counter added

--- app.rb
+++ app.rb
@@ -6,5 +6,54 @@ require './models.rb'
 before do
   if Count.count == 0
     Count.create(number: 0)
+  elsif Count.count ==1
+    Count.create(number: 1)
   end
+end
+get '/' do
+  @number = Count.first.number
+  @number2 = Count.second.number
+  erb:index
+end
+
+post '/plus' do
+  count = Count.first
+  count.number = count.number + 1
+  count.save
+  redirect "/"
+end
+
+post '/minus' do
+  count = Count.first
+  count.number = count.number - 1
+  count.save
+  redirect "/"
+end
+
+post '/clear' do
+  count = Count.first
+  count.number = 0
+  count.save
+  redirect "/"
+end
+  
+post '/multTwo' do
+  count = Count.first
+  count.number = count.number * 2
+  count.save
+  redirect "/"
+end
+
+post '/divTwo' do
+  count = Count.first
+  count.number = count.number / 2
+  count.save
+  redirect "/"
+end
+
+post '/plus2' do
+  count = Count.second
+  count.number = count.number + 1
+  count.save
+  redirect "/"
 end
